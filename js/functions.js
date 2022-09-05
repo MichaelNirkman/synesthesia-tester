fea21ac7482dad177010806f619907b7871c2a08
MikkoNirkko <abekebab@gmail.com>
2022-09-05 23:07:43 +0300

    finished results

--- js/functions.js
+++ js/functions.js
@@ -27,6 +27,9 @@ function makeSelection(color) {
         localStorage.setItem("currentindex", currentIndex);
         setCurrentNumber();
     } else {
+        currentChoices.push(color);
+        localStorage.setItem("currentchoices", JSON.stringify(currentChoices));
+        localStorage.setItem("currentindex", currentIndex);
         addToTotal(currentChoices);
         removeColors();
         displayThankYou();
@@ -35,9 +38,14 @@ function makeSelection(color) {
 
 function addToTotal(choices) {
     var allresults = JSON.parse(localStorage.getItem("allchoices"));
-    allresults.push(choices);
+    for (var i = 0; i < choices.length + 1; i++) {
+        if (allresults[i]) {
+            allresults[i].push(choices[i]);
+        } else {
+            allresults[i] = [choices[i]];
+        }
+    }
     localStorage.setItem("allchoices", JSON.stringify(allresults));
-    console.log(allresults);
 }
 
 function drawColors() {
@@ -93,20 +101,89 @@ function buildResults() {
 
     for (var i = settings["start-number"]; i < settings["end-number"] + 1; i++) {
         var entry = document.createElement("div");
+        entry.className = "result-row";
 
         var choicelist = [];
-        allitems.forEach(function(item) {
-            choicelist.push(item[i - 1]);
+
+        var colorRow = allitems[i - 1];
+        choicelist.push(getPercentages(colorRow));
+
+        var choiceTitle = createChoiceTitle(i);
+        entry.appendChild(choiceTitle);
+
+        choicelist.forEach(function(choices) {
+            for (
+                var u = settings["start-number"]; u < settings["end-number"] + 1; u++
+            ) {
+                var choice;
+                if (choices[u - 1]) {
+                    choice = choices[u - 1];
+                } else {
+                    choice = { color: "none", percentage: "" };
+                }
+
+                var choiceElement = createColorBlock(choice.color, choice.percentage);
+                entry.appendChild(choiceElement);
+            }
         });
 
-        var mostcommon = document.createElement("span");
-        mostcommon.className = "result-box";
-        mostcommon.style.backgroundColor = mode(choicelist);
-        mostcommon.innerHTML = i;
-        entry.appendChild(mostcommon);
+        entry.appendChild(document.createElement("br"));
 
         resultarea.appendChild(entry);
     }
+    var backbutton = document.createElement("button");
+    backbutton.innerHTML = "Return";
+    backbutton.className = "reset-button";
+    backbutton.onclick = function() {
+        resetForm();
+    };
+
+    resultarea.appendChild(backbutton);
+}
+
+function removeResults() {
+    var resultarea = document.getElementById("result-area");
+    document.getElementById("results").style.display = "none";
+    resultarea.innerHTML = "";
+}
+
+function createChoiceTitle(text) {
+    var titleItem = document.createElement("h2");
+    titleItem.className = "result-title";
+    titleItem.innerHTML = text;
+    return titleItem;
+}
+
+function createColorBlock(color, text) {
+    var element = document.createElement("span");
+    element.className = "result-box";
+    element.style.backgroundColor = color;
+    element.innerHTML = text;
+    return element;
+}
+
+function getPercentages(colors) {
+    var processedColors = [];
+    const totalItems = colors.length;
+    const uniqueItems = [...new Set(colors)];
+    uniqueItems.forEach((currColor) => {
+        const numItems = colors.filter((color) => color === currColor);
+        const percentage = Math.round((numItems.length * 100) / totalItems);
+        processedColors.push({
+            color: currColor,
+            percentage: percentage + "%",
+            percentageInt: percentage,
+        });
+    });
+    processedColors.sort((a, b) =>
+        a.percentageInt < b.percentageInt ?
+        1 :
+        b.percentageInt < a.percentageInt ?
+        -1 :
+        0
+    );
+
+    return processedColors;
 }
 
 function mode(arr) {
@@ -125,6 +202,7 @@ function setCurrentNumber() {
 
 function resetForm() {
     removeThankYou();
+    removeResults();
     restoreColors();
     initialize();
 }
